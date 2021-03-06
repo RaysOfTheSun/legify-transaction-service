7049cc7490770ce2163c2935f1fa1c3450279f31
Carl <rdelsol35@gmail.com>
2021-03-06 16:38:44 +0800

    initialize project sources

---
+++ src/main/java/com/raysofthesun/legifytransactionservice/LegifyTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raysofthesun.legifytransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegifyTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegifyTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raysofthesun/legifytransactionservice/LegifyTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raysofthesun.legifytransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegifyTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
